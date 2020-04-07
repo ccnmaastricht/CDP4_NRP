a4ad53021170633d641c93ae3e8149eddddb8af0
Mahmoud Akl <mahmoud.akl@gmail.com>
2020-04-07 13:11:52 +0200

    Added two IBA modules

--- install.sh
+++ install.sh
@@ -13,6 +13,26 @@ create_overwrite() {
     fi
 }
 
+# install pip packages
+pip install --upgrade pip
+pip install onnx tensorflow-addons
+if [ ! -d onnx-tensorflow ]; then
+    git clone https://github.com/onnx/onnx-tensorflow.git && cd onnx-tensorflow
+    pip install -e .
+    cd ../
+fi
+
+# Copy Models to local NRP
 create_overwrite "$HBP/Models/cdp4_objects"
 cp -r objects/* $HBP/Models/cdp4_objects
 $HBP/Models/create-symlinks.sh
+
+# Copy cdp4 GazeboRosPackages to local NRP
+cp -r GazeboRosPackages/cdp4_scene_understanding/ $HBP/GazeboRosPackages/src/
+
+# Copy individual modules to local NRP
+cp -r Object_recognition/ $HBP/GazeboRosPackages/src/cdp4_scene_understanding/
+
+# Build local GazeboRosPackages
+cd $HBP/GazeboRosPackages/
+catkin_make
